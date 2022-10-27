e2a25dde05145a63363e2e305a862a540a73f9be
Tanise Santos <tanise234@gmail.com>
2022-10-26 22:41:28 -0300

    🩹fix: lint

--- app/backend/src/routes/Router.ts
+++ app/backend/src/routes/Router.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router } from 'express';
 import { validate } from '../middlewares/loginMiddleware';
 
 const loginRouter = Router();
